7172c0bd731a74d23f6d1f7f6db1f301f72fd762
jdporr <jdporrazzo@gmail.com>
2024-02-06 16:56:11 -0500

    added saveBtn and local storage features

--- Assets/script.js
+++ Assets/script.js
@@ -18,11 +18,11 @@ $(".time-block").each(function(){
 
 //use $(this) to select all '.time-block' from DOM, pull the id attibute data, create an array for each dataset, 
 //remove the seperator and pull the data indexed at '1' in the respective array
-  let timeSlot = $(this).attr("id").split("-")[1];
-console.log(timeSlot); 
-
+  let timeSlot = $(this).attr("id").split("-")[1]; //["hour", "9"] 
+//console.log(timeSlot); 
+  let timeKey = $(this).attr("id")
 //Pull saved data from local storage and display in appropriate timeSlot
-  let textEntry = localStorage.getItem(timeSlot);
+  let textEntry = localStorage.getItem(timeKey);
   let textArea = $(this).find(".description");
   textArea.val(textEntry);
 //console.log(textArea);
@@ -37,8 +37,13 @@ console.log(timeSlot);
 });
 
 
+$(".saveBtn").on("click", function(){
+  var divKey = $(this).parent().attr("id")
+  var textValue = $(this).siblings(".description").val()
 
-
+  localStorage.setItem(divKey,textValue)
+});
+//$("#hour-16 .description").val(localStorage.getItem("hour-16"))
 
 displayDate();
 
